325622da90a4c136318f81928a9f5f7e24339004
Hafiza-Amna <hafizaamnanaseem1725@gmail.com>
2024-07-31 20:02:09 +0500

    Add files via upload

---
+++ # Assignment 1.py
@@ -0,0 +1,12 @@
+# Assignment 1
+print('''Myself Essay
+     Hello! My name is Hafiza Amna Naseem.
+      I am from Faisalabad.
+      I am  currently 20 years old.
+      I am studying BS-CS at UAF.
+      I enjoy to recite the Holy Quran daily.
+      In the future, I hope to develop something new in the field of AI.
+      I am very passionate about this field.
+      Iam new in this field.
+      Thank you for reading my essay!'''
+       ) 
